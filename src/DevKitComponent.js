21b60925b89cd6a0760a361e7212d8854ed7fc6a
Sade Siren <sade@lingsoft.fi>
2019-03-29 11:16:24 +0200

    init commit of skeletion plugin

---
+++ src/DevKitComponent.js
@@ -0,0 +1,82 @@
+import {Component} from 'substance'
+import {UIButton} from 'writer'
+
+class DevKitComponent extends Component {
+
+    /**
+     * Constructor
+     * @param args
+     */
+    constructor(...args) {
+        super(...args)
+    }
+
+    /**
+     * Method called when component is disposed and removed from DOM
+     */
+    dispose() {
+        // Perfect place to remove eventlisteners etc
+    }
+
+    /**
+     * Return the inital component state before rendering
+     *
+     * @returns {{clickCount: number}}
+     */
+    getInitialState() {
+        return {
+            clickCount: 0
+        }
+    }
+
+    /**
+     * Do something after the first render
+     */
+    didMount() {
+        console.log('Devkit plugin rendered')
+    }
+
+    /**
+     * Render method is called whenever there's a change in state or props
+     *
+     * @param $$
+     * @returns {*}
+     */
+    render($$) {
+        const el = $$('div')
+            .addClass('devkit')
+
+        const button = $$(UIButton, {
+            label: this.getLabel('Click me')
+        })
+            .on('click', () => {
+                this.increaseClickCount()
+            })
+
+        el.append([
+            $$('h2').append(
+                this.getLabel('Devkit plugin loaded')
+            ),
+            $$('p').append(
+                this.getLabel('Number of clicks') + `: ${this.state.clickCount}`
+            ),
+            button
+        ])
+
+        return el
+    }
+
+    /**
+     * Increase click count by updating the state.
+     *
+     * The method extendState() perform a partial update of the state, if
+     * you want to completely replace the state, use setState() instead.
+     */
+    increaseClickCount() {
+        this.extendState({
+            clickCount: this.state.clickCount + 1
+        })
+    }
+}
+
+export {DevKitComponent}
